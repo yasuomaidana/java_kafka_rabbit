1c6c1aa4ad166e125bf8bfe07094938d5dfc32cd
yasuomaidana <yasuomaidana@gmail.com>
2025-04-23 17:42:21 -0600

    Adding flyway dependencies and plugins

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
     id("java-library")
+    id("org.flywaydb.flyway") version "9.22.0"
+
+    id("org.springframework.boot") version "3.4.4"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.vulpuxmecatroncus"
@@ -7,11 +11,24 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://download.red-gate.com/maven/release")
+    }
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // Spring Data JPA
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
+
+    implementation("org.postgresql:postgresql:42.7.1")
+    implementation("org.flywaydb:flyway-core:9.22.0")
+
+
+    // Lombok
     implementation("org.projectlombok:lombok:1.18.30")
     annotationProcessor("org.projectlombok:lombok:1.18.30")
 }
